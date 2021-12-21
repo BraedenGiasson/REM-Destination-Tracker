42d40a3f76a067f0f055c2e7dddcd7988437786d
Braeden Giasson <2043670@johnabbottcollege.net>
2021-12-21 12:31:21 -0500

    Updated ui

--- project website/src/project.js
+++ project website/src/project.js
@@ -21,8 +21,11 @@ async function GettingAllStations(){
 
 async function TestSchedule(){
 
-    let fetching = await fetch( "http://10.101.0.12:8080/schedule/Brossard" );
-
+    // let fetching = await fetch( "http://10.101.0.12:8080/schedule/Brossard" );
+    // let fetching = await fetch( " http://10.101.0.12:8080/notifications/22" );
+    // let fetching = await fetch( " http://10.101.0.12:8080/schedule/Sainte-Dorothée" );
+    let fetching = await fetch( " http://10.101.0.12:8080/distance/Sainte-Dorothée/Bois-Franc" );
+    // let fetching = await fetch( " http://10.101.0.12:8080/schedule/Sainte-Dorothée" 
     let responseFromFetch = await fetching.json();
 
     console.log(responseFromFetch);
