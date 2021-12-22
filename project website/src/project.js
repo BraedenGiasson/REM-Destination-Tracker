31e585030f590903693f850cc2775dbd7a243fae
Braeden Giasson <2043670@johnabbottcollege.net>
2021-12-22 13:30:32 -0500

    Added colors for segment change for path

--- project website/src/project.js
+++ project website/src/project.js
@@ -25,6 +25,7 @@ let getStartStation = document.querySelector('#start');
 let getEndStation = document.querySelector('#end');
 
 let getNumStationsOnPath = 0;
+let getAllStationsOnPath = null;
 
 async function GetStationsPath(){
 
@@ -37,6 +38,8 @@ async function GetStationsPath(){
     let responseFromFetch = await fetching.json();
 
     getNumStationsOnPath = responseFromFetch.length;
+
+    getAllStationsOnPath = responseFromFetch;
 
     console.log(responseFromFetch);
     GetDistanceEachStation( responseFromFetch );
@@ -78,7 +81,7 @@ async function GetStartStationSchedule(){
 
 async function GetDistanceEachStation( stationsOnPath ){
     let counter = 0;
-    
+    console.log(stationsOnPath);
     for (let i = 0; i < stationsOnPath.length; i++) {
         
         if (i == 0){
@@ -114,7 +117,8 @@ async function GetDistanceEachStation( stationsOnPath ){
 
             console.log("time " + timee);
 
-            let newStationClass = new Station(stationsOnPath[i + 1].Name, getTimeTaken);
+            let newStationClass = new Station(stationsOnPath[i].StationId, stationsOnPath[i].SegmentId, 
+                stationsOnPath[i + 1].Name, getTimeTaken);
             array.push(newStationClass);
         }
         else if(i > 1){
@@ -150,8 +154,11 @@ async function GetDistanceEachStation( stationsOnPath ){
 
             console.log("time " + timee);
 
-            let newStationClass = new Station(stationsOnPath[i].Name, getTimeTaken);
-            array.push(newStationClass);
+            if (getAllStationsOnPath[i].StationId !== getAllStationsOnPath[i - 1].StationId){
+                let newStationClass = new Station(stationsOnPath[i].StationId, stationsOnPath[i].SegmentId, 
+                    stationsOnPath[i].Name, getTimeTaken);
+                array.push(newStationClass);
+            }
         }
     }
     console.log("final counter " + counter);
@@ -159,6 +166,7 @@ async function GetDistanceEachStation( stationsOnPath ){
 
     console.log(array);
     CreateGrid();
+    console.log(getAllStationsOnPath);
 }
 
 async function CalculateTime( distance ){
@@ -183,12 +191,13 @@ async function CalculateTime( distance ){
     console.log("time " + timee);
 }
 
-
+let isDifferent = false;
 function CreateGrid(){
 
     let getGrid = document.querySelector('.grid-container');
     getGrid.style.gridTemplateRows = "repeat(" + getNumStationsOnPath + ", 100px )";
     console.log(getGrid.style.gridTemplateRows.length);
+    // let isDifferent = false;
 
     for (let i = 0; i < array.length; i++) {
         let theName = document.createElement("div");
@@ -204,8 +213,14 @@ function CreateGrid(){
         theTime.style.gridColumn = 3;
         getGrid.appendChild(theTime);
 
-        CreateLine( getGrid, i );
-        CreateRectangle( getGrid, i );
+        if(i > 0){
+            if (array[i].SegmentId !== array[i - 1].SegmentId){
+                isDifferent = true;
+            }
+        }
+
+        CreateLine( getGrid, i, isDifferent );
+        CreateRectangle( getGrid, i, isDifferent );
 
         //let createCanvas = document.createElement('canvas');
         // createCanvas.width = 30;
@@ -245,7 +260,7 @@ function CreateGrid(){
     }
     
 }
-function CreateLine( getGrid, i ){
+function CreateLine( getGrid, i, isDifferent ){
     let createCanvas = document.createElement('canvas');
     createCanvas.width = 30;
     createCanvas.height = 100;
@@ -254,6 +269,14 @@ function CreateLine( getGrid, i ){
     canvas2d.moveTo(2,0);
     canvas2d.lineWidth = 2;
     canvas2d.lineTo(0, 300);
+
+    if (isDifferent){
+        canvas2d.strokeStyle = "#1F06FE";
+    }
+    else{
+        canvas2d.strokeStyle = "black";
+    }
+
     canvas2d.stroke();
     document.body.appendChild(createCanvas);
     createCanvas.style.gridColumn = 2;
@@ -261,12 +284,19 @@ function CreateLine( getGrid, i ){
     createCanvas.style.marginLeft = "40%";
     getGrid.appendChild(createCanvas);
 }
-function CreateRectangle( getGrid, i ){
+function CreateRectangle( getGrid, i, isDifferent ){
     let createCanvas = document.createElement('canvas');
     createCanvas.width = 30;
     createCanvas.height = 100;
     let canvas2d = createCanvas.getContext('2d');
-    canvas2d.fillStyle = "black";
+
+    if (isDifferent) {
+        canvas2d.fillStyle = "#1F06FE";
+    }
+    else{
+        canvas2d.fillStyle = "black";
+    }
+    
     canvas2d.fillRect(0,0,7,7)
     document.body.appendChild(createCanvas);
     createCanvas.style.gridColumn = 2;
@@ -278,15 +308,23 @@ function CreateRectangle( getGrid, i ){
 
 class Station
 {
-    constructor ( name, time )
+    constructor ( stationId, segmentId, name, time )
     {
         this.name = name;
         this.time = time.toFixed(2);
+        this.stationId = stationId;
+        this.segmentId = segmentId;
     }
     get Name(){
         return this.name;
     }
     get Time(){
         return this.time;
+    }
+    get StationId(){
+        return this.stationId;
+    }
+    get SegmentId(){
+        return this.segmentId;
     }
 }
