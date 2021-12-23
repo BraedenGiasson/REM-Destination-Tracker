9ca206c2d201df6ea6cf498866d9459ddce5d68d
Braeden Giasson <2043670@johnabbottcollege.net>
2021-12-23 17:16:32 -0500

    Added comments

--- project website/src/project.js
+++ project website/src/project.js
@@ -214,36 +214,7 @@ function GetMatchingHours( theHours, theSchedule, arrayToAddTo ){
     }
 }
 
-// function GetMatchingHours( hrs ){
-
-//     // Get the times where the hours match the input time hours
-//     for (let r = 0; r < startStationSchedule.length; r++) {
-
-//         // Splitting the time string to get time after char T
-//         let getRidOfFront = startStationSchedule[r].Time.toString().split("T");
-//         // Splitting the time string to get time before char .
-//         let getRidOfBack = getRidOfFront[1].split(".");
-
-//         // Temp date to get matching time hours
-//         let dummyDate = new Date(getRidOfFront[0] + " " + getRidOfBack[0]);
-        
-//         // If hours match from temp date and hours from input, push to array
-//         if (dummyDate.getHours() === parseInt(hrs)){            
-//             getHoursFromStation.push(getRidOfFront[0] + " " + getRidOfBack[0]);
-//         }
-//         // Getting the first time for the next hour (in case input hour is that last for that hour)  
-//         else if (dummyDate.getHours() === (parseInt(hrs) + 1)){
-//             getHoursFromStation.push(getRidOfFront[0] + " " + getRidOfBack[0]);
-//             return;
-//         }
-
-//         // for my own use
-//         console.log(getRidOfFront);
-//         console.log(getRidOfBack);
-//         console.log(dummyDate.getHours(), (parseInt(hrs) + 1));
-//         console.log(dummyDate.toLocaleTimeString("en-GB"), dummyDate, parseInt(hrs), startStationSchedule[r].Time);
-//     }
-// }
+//#endregion
 
 //#region Getting closest time to input time
 function GetClosestTimeToInputTime( mins, arrayToAddTo ){
@@ -289,49 +260,6 @@ function GetClosestTimeToInputTime( mins, arrayToAddTo ){
     return indexOfStartingTime;
 }
 
-// function GetClosestTimeToInputTime( mins, arrayToAddTo ){
-
-//     let indexOfStartingTime = -1;
-//     let currentClosest = null;
-
-//     // Getting closest time to input time
-//     for (let t = 0; t < getHoursFromStation.length; t++) {
-
-//         // Creating date from current station on path
-//         let newDateInArray = new Date(getHoursFromStation[t]);
-
-//         // Intializing currentClosest to large number
-//         if (t === 0){
-//             currentClosest = 100;
-//         }
-
-//         // If the times match (time to leave now), return the current index
-//         if (parseInt(mins) === newDateInArray.getMinutes()){
-//             return t;
-//         }
-//         else{
-//             // Getting the difference in minutes from input time and current temp date
-//             let dateDiff = (parseInt(mins) - newDateInArray.getMinutes());
-
-//             // If difference is negative, return index (meaning next closest time)
-//             // OR If the time is in the next hour, return index
-//             if(dateDiff < 0 || t === (getHoursFromStation.length - 1)){
-//                 return t;
-//             }
-//             // If closest time is greater than the difference, set current closest to difference
-//             if (currentClosest > dateDiff){
-//                 currentClosest = dateDiff;
-//                 indexOfStartingTime = t;
-//             }
-//         }
-
-//         // for my own use
-//         console.log(newDateInArray, getTimeHTML.value);
-//         console.log(parseInt(mins), newDateInArray.getMinutes()); 
-//     }
-//     return indexOfStartingTime;
-// }
-
 //#endregion
 
 //#region Getting the distance between each station on the path
@@ -349,7 +277,6 @@ async function GetDistanceEachStation( stationsOnPath ){
         
         // Creating date from the closest time to the input time
         let timeDate = new Date(getClosestTime);
-        let newTimeDate
         
         // If first index, start at closest time
         if (i === 0){
@@ -357,7 +284,7 @@ async function GetDistanceEachStation( stationsOnPath ){
             let splittingByColon = timeDate.toLocaleTimeString("en-GB").toString().split(":");
             let newFormatedTime = splittingByColon[0] + ":" + splittingByColon[1];
 
-            // Start at closest time
+            // adding to array
             let newStationClass = new Station(stationsOnPath[i].StationId, stationsOnPath[i].SegmentId, 
                 stationsOnPath[i].Name, newFormatedTime);
             arrayOfStationsOnPath.push(newStationClass);
@@ -380,45 +307,46 @@ async function GetDistanceEachStation( stationsOnPath ){
             let getSpeedInMinutesForHour = averageSpeed/60;
             let getTimeTaken = (responseFromFetch/averageSpeed) * 60;
 
-            console.log(isSwitchedSegment);
+            console.log(isSwitchedSegment); // Logs for my own use
 
             // If there's not a duplicate (back-to-back) station, add to array
             if (getAllStationsOnPath[i].StationId !== getAllStationsOnPath[i - 1].StationId){
 
+                // Checking if switched segment
                 if(isSwitchedSegment){
 
                     // Creating date from the closest time to the input time
                     let newTimeDate = new Date(newClosestTime);
-                    console.log(newTimeDate);
-                    console.log(arrayOfStationsOnPath);
-                    
+                   
                     // setting new added minutes from time between stations
+                    // If it's first station on segment, new date with closest time
                     if (isFirstStationOnNewSegment){
-                        console.log(newClosestTime);
                         newTimeDate = new Date(newClosestTime);
                         isFirstStationOnNewSegment = false;
-                        console.log("first time now false");
                     }
+                    // If it's not first station on segment
                     else{
-                        console.log("nopr");
-                        console.log(stationsOnPath.length);
-                        console.log(i);
-                        console.log(arrayOfStationsOnPath[i - 2]);
-                        console.log(arrayOfStationsOnPath[i - 2].time);
                         newTimeDate = new Date(getDateHTML.value + " " + arrayOfStationsOnPath[i - 2].time);
                     }
                    
+                    // adding minutes to time
                     newTimeDate.setMinutes( newTimeDate.getMinutes() + Math.ceil(getTimeTaken) )
                     
                     // Formating time to just show hours and minutes
                     let formatTime = newTimeDate.toLocaleTimeString("en-GB");
                     let splittingByColon = formatTime.toString().split(":");
                     let newFormatedTime = splittingByColon[0] + ":" + splittingByColon[1];
-                    console.log(getAllStationsOnPath);
 
+                    // Adding to array of stations on path
                     let newStationClass = new Station(stationsOnPath[i].StationId, stationsOnPath[i].SegmentId, 
                         stationsOnPath[i].Name, newFormatedTime);
                     arrayOfStationsOnPath.push(newStationClass);
+
+                    // Logs for my own use
+                    console.log(newTimeDate);
+                    console.log(arrayOfStationsOnPath);
+                    console.log(newClosestTime);
+                    console.log(getAllStationsOnPath);
                 }
                 else{
 
@@ -432,15 +360,13 @@ async function GetDistanceEachStation( stationsOnPath ){
                     let newFormatedTime = splittingByColon[0] + ":" + splittingByColon[1];
                     console.log(getAllStationsOnPath);
 
+                    // Adding to array of stations on path
                     let newStationClass = new Station(stationsOnPath[i].StationId, stationsOnPath[i].SegmentId, 
                         stationsOnPath[i].Name, newFormatedTime);
                     arrayOfStationsOnPath.push(newStationClass);
                 }
             }
             else{
-
-                console.log("here");
-                console.log(arrayOfStationsOnPath);
 
                 if (getAllStationsOnPath[i].SegmentId !== getAllStationsOnPath[i- 1].SegmentId){
                     console.log(getAllStationsOnPath[i + 1].SegmentId);
@@ -455,9 +381,6 @@ async function GetDistanceEachStation( stationsOnPath ){
                     let getScheduleWithOnlyRightSegmentId = responseFromNewScheduleFetch.filter(element => element.SegmentId === segmentIdToSearchFor);
 
                     let getPreviousTime = arrayOfStationsOnPath[i - 1].time + ":00"; 
-                    console.log(getPreviousTime);
-                    console.log(getScheduleWithOnlyRightSegmentId);
-                    let staticTime = "1970-01-01";
 
                     let hrsFromPreviousTime = getPreviousTime.split(":")[0];
                     let minsFromPreviousTime = getPreviousTime.split(":")[1];
@@ -465,17 +388,20 @@ async function GetDistanceEachStation( stationsOnPath ){
                     GetMatchingHours( hrsFromPreviousTime, getScheduleWithOnlyRightSegmentId, getNewHoursFromStation );
 
                     let indexNewClosestTime = GetClosestTimeToInputTime( minsFromPreviousTime, getNewHoursFromStation );
-                    console.log(indexNewClosestTime);
-                    console.log(getNewHoursFromStation);
 
                     newClosestTime = getNewHoursFromStation[indexNewClosestTime]; // setting closest time
 
-                    console.log(newClosestTime);
-                    console.log(responseFromNewScheduleFetch);
-
                     isSwitchedSegment = true;
                     isFirstStationOnNewSegment = true;
-                    console.log("now true");
+
+                    // Logs for my own use
+                    console.log(arrayOfStationsOnPath);
+                    console.log(getPreviousTime);
+                    console.log(getScheduleWithOnlyRightSegmentId);
+                    console.log(indexNewClosestTime);
+                    console.log(getNewHoursFromStation);
+                    console.log(newClosestTime);
+                    console.log(responseFromNewScheduleFetch);
                 }
             }
 
@@ -554,6 +480,7 @@ function CreateDetailsForColumnTitles( getNumberOfColumns, columnTitles ){
             currentTitleElement.style.placeSelf = "end start";
             currentTitleElement.style.marginLeft = "10px";
         }
+        // Positioning title
         else if (l === fourthColumn){
             currentTitleElement.style.textAlign = "start";
             currentTitleElement.style.placeSelf = "end center";
@@ -648,22 +575,12 @@ async function CreateNameAndTime(){
         }
 
         if(i > 1){
-            console.log(arrayOfStationsOnPath);
-            //console.log(arrayOfStationsOnPath[i].name + " " + arrayOfStationsOnPath[i - 1].name);
+            
             // If the segment id is different to the previous, set to true (changing segments)
-            //console.log(arrayOfStationsOnPath[i] + " " + arrayOfStationsOnPath[i - 1]);
-            console.log(i);
-            console.log(arrayOfStationsOnPath[i - 1].segmentId + " " + arrayOfStationsOnPath[i - 2].segmentId);
             if (arrayOfStationsOnPath[i - 1].segmentId !== arrayOfStationsOnPath[i - 2].segmentId){
                 isDifferent = true;
-                console.log("yes true");
             }
         }
-
-        //let fetchWeather = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Montreal?unitGroup=metric&key=63LJ2AXSUK5V5VEBYN9JRZY6Y&contentType=json");
-        // let fetchWeather = await fetch("http://api.weatherapi.com/v1/current.json?key=aab951d047cd455d85d164045212312 &q=London&aqi=yes");
-        //let responseF = await fetchWeather.json();
-        //console.log(responseF);
 
         // If it's the last index, don't create extra line,
         // else, create the line between stations
@@ -739,11 +656,14 @@ function SettingCanvasWidthHeight( createCanvas ){
 
 //#endregion
 
+//#region Filling canvas
+
 function FillCanvasBasedOnIfDifferent( canvas2d, isDifferent, styling ){
 
     // If the segment is different, change the color to blue; else, keep black
     if (isDifferent) {
 
+        // If the canvas is the rectangle
         if (styling == "fill"){
             canvas2d.fillStyle = "#1F06FE";
         }
@@ -753,6 +673,7 @@ function FillCanvasBasedOnIfDifferent( canvas2d, isDifferent, styling ){
     }
     else{
 
+        // If the canvas is the line
         if (styling == "fill"){
             canvas2d.fillStyle = "black";
         }
@@ -761,6 +682,8 @@ function FillCanvasBasedOnIfDifferent( canvas2d, isDifferent, styling ){
         }
     } 
 }
+
+//#endregion
 
 //#region Creating details for canvas (line and dot)
 
